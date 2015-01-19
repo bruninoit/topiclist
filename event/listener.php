23f0b9e04dc990d150bd240990d3529cc9e79a9f
bruninoit <brunino98@hotmail.it>
2015-01-19 14:05:33 +0100

    completato forum_id

--- event/listener.php
+++ event/listener.php
@@ -79,12 +79,11 @@ public function viewtopic_add($event)
 $rowmessage=$event['post_row'];
 $message=$rowmessage['MESSAGE'];
 $post_id=$rowmessage['POST_ID'];
-//query
-$lista_topics=$this->db->sql_query("SELECT forum_id
+$forum_query=$this->db->sql_query("SELECT forum_id
     FROM " . POSTS_TABLE . "
     WHERE post_id = $post_id");
-    
-$forum_id=13;//ris query
+    $forum_id_array=$this->db->sql_fetchrow($forum_query)
+$forum_id=$forum_id_array[forum_id];
 define("TOPIC_LIST", "Topic List");
 define("TOPIC_TITLE", "Titolo");
 define("TOPIC_AUTHOR", "Autore");
